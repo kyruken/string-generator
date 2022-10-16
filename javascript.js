a7bca84c8b90267462e325a0a407bb32216cf8ec
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-15 22:19:43 -0700

    fixed words choosing functionality works

--- javascript.js
+++ javascript.js
@@ -7,12 +7,18 @@ async function setWordsArray() {
     .then((data) => wordsArray = data);
     console.log(wordsArray);
 }
-
+addGenerateEvent();
 setWordsArray();
 
+function addGenerateEvent() {
+    const generateButton = document.getElementById('generaterandom');
+    generateButton.addEventListener('click', function(e){
+        e.preventDefault();
+        createRandomWords(getListInput());
+    })
+}
 
-function createRandomWords() {
-    let amountOfWords = getListInput();
+function createWords(amountOfWords) {
     const wordContainer = document.createElement('div');
     for (let x = 0; x < amountOfWords; x++) {
         let newWord = getRandomWord();
@@ -23,6 +29,23 @@ function createRandomWords() {
 
     document.body.appendChild(wordContainer);
 }
+
+function getFixedWords(amountOfWords, letter) {
+    const wordContainer = document.createElement('div');
+    let wordsAppended = 0;
+    for (let x = 0; amountOfWords != wordsAppended; x++) {
+        if (wordsArray[x].charAt(0) === letter) {
+            let pElement = document.createElement('p');
+            pElement.textContent = wordsArray[x];
+            wordContainer.appendChild(pElement);
+            wordsAppended++;
+        }
+    }
+    
+    document.body.appendChild(wordContainer);
+
+}
+
 //For first words box input
 function getListInput() {
     const randomListInput = document.getElementById('words');
@@ -34,49 +57,21 @@ function getRandomWord() {
     return wordsArray[randomNumber];
 }
 
-function getNumberofWords() {
+
+function termsFieldSet() {
     const termsFieldSet = document.getElementById('termsfieldset');
     const checkboxInputs = termsFieldSet.querySelectorAll('input[type=checkbox]');
-    const numberInputs = termsFieldSet.querySelectorAll('input[type=number]');
 
     checkboxInputs.forEach(input => {
         if (input.checked === true) {
+            let inputLetter = input.id;
+            let numberInput = input.nextElementSibling;
+            let inputValue = numberInput.value;
+            console.log('went through checkbox inputs');
+
+            getFixedWords(inputValue, inputLetter);
             
         }
     })
-
-    numberInputs.forEach(input => {
-        if(input.value > 0) {
-            console.log(input.value);
-        }
-    })
-
-
-    // const numOfA = document.getElementById('anum');
-    // const numOfB = document.getElementById('bnum');
-    // const numOfC = document.getElementById('cnum');
-    // const numOfD = document.getElementById('dnum');
-    // const numOfE = document.getElementById('enum');
-    // const numOfF = document.getElementById('fnum');
-    // const numOfG = document.getElementById('gnum');
-    // const numOfH = document.getElementById('hnum');
-    // const numOfI = document.getElementById('inum');
-    // const numOfJ = document.getElementById('jnum');
-    // const numOfK = document.getElementById('knum');
-    // const numOfL = document.getElementById('lnum');
-    // const numOfM = document.getElementById('mnum');
-    // const numOfN = document.getElementById('nnum');
-    // const numOfO = document.getElementById('onum');
-    // const numOfP = document.getElementById('pnum');
-    // const numOfQ = document.getElementById('qnum');
-    // const numOfR = document.getElementById('rnum');
-    // const numOfS = document.getElementById('snum');
-    // const numOfT = document.getElementById('tnum');
-    // const numOfU = document.getElementById('unum');
-    // const numOfV = document.getElementById('vnum');
-    // const numOfW = document.getElementById('wnum');
-    // const numOfX = document.getElementById('xnum');
-    // const numOfY = document.getElementById('ynum');
-    // const numOfZ = document.getElementById('znum');
-
 }
+
